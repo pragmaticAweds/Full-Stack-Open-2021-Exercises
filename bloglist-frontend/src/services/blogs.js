147991840d14f6d467d79aa59b630c53a65c3c5b
Abdul Afeez Aweda <jimohafeez738@gmail.com>
2022-02-25 15:42:44 +0100

    update

--- bloglist-frontend/src/services/blogs.js
+++ bloglist-frontend/src/services/blogs.js
@@ -7,9 +7,9 @@ const setToken = (newToken) => {
   token = `bearer ${newToken}`;
 };
 
-const getAll = () => {
-  const request = axios.get(baseUrl);
-  return request.then((response) => response.data);
+const getAll = async () => {
+  const response = await axios.get(baseUrl);
+  return response.data;
 };
 
 const create = async (newObject) => {
@@ -29,9 +29,9 @@ const deleteObj = async (id) => {
   return response.data;
 };
 
-const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject);
-  return request.then((response) => response.data);
+const update = async (id, newObject) => {
+  const response = await axios.put(`${baseUrl}/${id}`, newObject);
+  return response.data;
 };
 
 // eslint-disable-next-line
