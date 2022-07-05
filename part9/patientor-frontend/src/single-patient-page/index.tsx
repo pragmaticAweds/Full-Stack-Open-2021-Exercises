630953258b9b80452de3e7ff3c3a0bf45019b6ca
Abdul-Afiz <jimohafeez738@gmail.com>
2022-07-05 07:04:45 +0100

    feat: integrated a new feature to display

--- part9/patientor-frontend/src/single-patient-page/index.tsx
+++ part9/patientor-frontend/src/single-patient-page/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { Typography } from "@material-ui/core";
 import { useStateValue } from "../state/state";
 import { Female, Male } from "@mui/icons-material";
@@ -6,13 +6,14 @@ import axios from "axios";
 import { apiBaseUrl } from "../constants";
 import { useParams } from "react-router-dom";
 import { setSinglePatient } from "../state";
+import { Entry } from "../types";
 
-function index() {
+function index(): React.ReactElement {
   const [{ patientRecords }, dispatch] = useStateValue();
 
   const { id } = useParams<{ id: string }>();
 
-  React.useEffect(() => {
+  useEffect(() => {
     const fetchSinglePatient = async () => {
       try {
         const { data } = await axios.get(`${apiBaseUrl}/patients/${id}`);
@@ -23,12 +24,12 @@ function index() {
     };
     fetchSinglePatient();
   }, [dispatch]);
-
+  console.log({ patientRecords });
   return (
     <div>
       {patientRecords !== null ? (
         <>
-          <Typography variant="h4" style={{ margin: "2rem 0" }}>
+          <Typography variant="h4" style={{ margin: "1rem 0" }}>
             {patientRecords.name}
             {patientRecords ? (
               patientRecords.gender === "female" ? (
@@ -44,6 +45,24 @@ function index() {
           <Typography variant="body1">
             occupation: {patientRecords.occupation}
           </Typography>
+          <Typography
+            variant="h5"
+            style={{ margin: "1.5rem 0", fontWeight: "700" }}
+          >
+            entries
+          </Typography>
+          <div>
+            {patientRecords.entries?.map((patient: Entry) => (
+              <div key={`entry_Key_${patient.id}}`}>
+                <Typography>{patient.description}</Typography>
+                <ul>
+                  {patient.diagnosisCodes?.map((code, i) => (
+                    <li key={`code_Key_${i}`}>{code}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
         </>
       ) : null}
     </div>
